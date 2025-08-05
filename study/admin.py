41d6352e52c665acdebc230e56e1317081a59d6a
Julitosh07 <julito240907@gmail.com>
2025-08-04 19:11:04 -0500

    visaulizar models en el admin

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Subtema, MaterialEstudio
 
 # Register your models here.
+@admin.register(Subtema)
+class SubtemaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'subject')
+    list_filter = ('subject',)            
+    search_fields = ('nombre',)           
+
+@admin.register(MaterialEstudio)
+class MaterialEstudioAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'subtema', 'tipo', 'visto')
+    list_filter = ('tipo', 'visto', 'subtema')
+    search_fields = ('titulo', 'descripcion')
